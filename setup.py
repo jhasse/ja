072ccc5a62a5366fddc84859cddb656d2ba75d54
Jan Niklas Hasse <jhasse@bixense.com>
2017-10-05 15:55:27 +0200

    Add frontend.pb as package_data

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     download_url='https://github.com/jhasse/ja/archive/v1.0.0.tar.gz',
     description="Frontend for Ninja focusing on a faster edit, compile, debug cycle",
     packages=['ja'],
+    package_data={'': ['*.pb']},
     entry_points={
         'console_scripts': ['ja = ja:main'],
     },
